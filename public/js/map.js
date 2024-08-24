4886a615d4e8067b2309166d16a6dcd5a7d7285a
K SOURAV KUMAR <cse.22bcsh16@silicon.ac.in>
2024-08-24 10:11:52 +0530

    Add Project Files

---
+++ public/js/map.js
@@ -0,0 +1,7 @@
+maptilersdk.config.apiKey = mapToken;
+const map = new maptilersdk.Map({
+container: 'map', // container's id or the HTML element to render the map
+style: maptilersdk.MapStyle.STREETS,
+center: [77.2090, 28.6139], // starting position [lng, lat]
+zoom: 9, // starting zoom
+});
